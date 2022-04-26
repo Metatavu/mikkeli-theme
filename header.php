2aa7a8ba309cea1b87ce5c9f0750046dba33d747
Sami Kallio <kalliosa@users.noreply.github.com>
2022-04-26 15:15:11 +0300

    Headings alt font and print styles

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-
+<link rel="stylesheet" href="https://use.typekit.net/jyx1iep.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@600;900&family=Titillium+Web:wght@400;700&display=swap" rel="stylesheet">
