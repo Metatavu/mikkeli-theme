c76696354f432387d56f0531eb689a2f15b123a7
Sami Kallio <sami@haaja.com>
2022-08-03 10:33:29 +0300

    G4 gtag

--- header.php
+++ header.php
@@ -19,17 +19,12 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@600;900&family=Titillium+Web:wght@400;700&display=swap" rel="stylesheet">
 
-<?php 
-	$ga_code = get_option('mikkeli_google_analytics'); 
+<?php
+	$ga_code = get_option('mikkeli_google_analytics');
   if($ga_code): ?>
-<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-	ga('create', '<?php echo $ga_code; ?>', 'auto');
-	ga('send', 'pageview');
-</script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $ga_code; ?>"></script>
+<script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '<?php echo $ga_code; ?>'); </script>
 <?php endif; ?>
 <!--
 <script type="text/javascript">window.__lc = window.__lc || {};
