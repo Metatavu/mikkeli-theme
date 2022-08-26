c6792aefb3f5370465c8dc6ba488c514ba336ee6
Sami Kallio <sami.kallio@kulmaus.com>
2022-08-26 11:00:42 +0300

    Link Font Weight, Default News Image

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 <link rel="stylesheet" href="https://use.typekit.net/jyx1iep.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@600;900&family=Titillium+Web:wght@400;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@600;900&family=Titillium+Web:wght@400;600;700&display=swap" rel="stylesheet">
 
 <?php
 	$ga_code = get_option('mikkeli_google_analytics');
