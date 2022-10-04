29adce639f78d33646f8d54791646600d9e5c30c
Antti Leppä <antti.leppa@metatavu.fi>
2022-10-04 12:12:08 +0300

    Removed extra space from Toimintaohjeet

--- sptv/components/service/user-instruction.php
+++ sptv/components/service/user-instruction.php
@@ -3,7 +3,5 @@
   include_once $paths["common"];
 
   echo "<h2>Toimintaohjeet</h2>";
-  echo "<p>";
   echo nl2p(getLocalizedValue($data->service["serviceDescriptions"], $data->language, "UserInstruction"));
-  echo "</p>";
 ?>
